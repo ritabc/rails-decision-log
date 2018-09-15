e8fdd79598682ebde55160962fa995671a8e9ded
ritabc <ritabc@gmail.com>
2018-09-15 00:44:02 -0700

    fix three_most_recent(circle) method - no errors

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -3,10 +3,10 @@ class Decision < ApplicationRecord
   validates_presence_of :name
   validates_presence_of :description
   validates_presence_of :date_decided
-  scope(:three_most_recent, -> do
+  scope(:three_most_recent, -> (circle) {
     binding.pry
-    where("circle_id = ?", Circle.find(self.circle_id).id)
+    where("circle_id = ?", circle.id)
     .order(date_decided: :desc)
     .limit(3)
-  end)
+  })
 end
