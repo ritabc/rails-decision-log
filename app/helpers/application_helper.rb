915a8caa89b801f7e19e3bd321d9088353a0a037
ritabc <ritabc@gmail.com>
2019-02-14 17:17:00 -0500

    Add and style sort-and-search navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,20 @@
 module ApplicationHelper
   def sortable(column, title)
-    if params[:search]
-      content_tag :span, title
+    if params[:search] #TODO: Needs helper if_should_show_all_or_search_results
+      content_tag :span, title, class: "nav-link"
     else
+      # Toggle sort_direction
       direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
-      link_to title, :sort => column, :direction => direction
+      # Logic for asc/desc sort icon
+      css_class = column == sort_column ? "current #{direction}" : nil
+
+      # Add Link to sort + sorted Icon
+      link_to url_for(controller: "decisions", action: "index", sort: column, direction: direction), :class => "nav-link #{css_class}" do
+        concat(content_tag :span, title, style: "color: #0000EE")
+        if css_class
+          concat(image_tag("/assets/sort_icons/#{sort_direction}.png", alt: "#{sort_direction} icon", size: "17x17"))
+        end
+      end
     end
   end
 end
