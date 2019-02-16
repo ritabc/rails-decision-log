ee44058be65e7239f117cdfc0da6bd59a01e9bcc
ritabc <ritabc@gmail.com>
2019-02-16 17:47:56 -0500

    Remove three-most-recent scope and update Circles#Index

--- app/models/decision.rb
+++ app/models/decision.rb
@@ -8,11 +8,11 @@ class Decision < ApplicationRecord
   pg_search_scope :custom_search, against: [:name, :description],
       using: {tsearch: {dictionary: "english"}}
 
-  scope(:three_most_recent, -> (circle) {
-    where("circle_id = ?", circle.id)
-    .order(date_decided: :desc)
-    .limit(3)
-  })
+  # scope(:three_most_recent, -> (circle) {
+  #   where("circle_id = ?", circle.id)
+  #   .order(date_decided: :desc)
+  #   .limit(3)
+  # })
 
   def self.name_description_search(search)
     if search.present?
