c98540b228ba3c0fe8e371e5ed3844de33c56142
ritabc <ritabc@gmail.com>
2019-02-18 19:11:15 -0500

    Continue with specs for Leader users who interact with decisions

--- app/models/permission.rb
+++ app/models/permission.rb
@@ -7,13 +7,13 @@ class Permission
     @decision = attributes.fetch(:decision)
   end
 
-  ## Authorizes. Does NOT account for whether user is associated with circle
   # TODO: Refactor when Done with all 'Modules' (Decisions/Circles,Users), and either organize by module, ie controller at top of nesting, or reflext organization of permission_spec.rb
   def allow?(controller, action)
     return true if controller == "sessions"
     return true if controller == "decisions" && action == "index"
 
     if user # if user is logged in...
+      return true if controller == "decisions" && action == "new"
       if user.super? # ...and if they're super
         return true if controller == "decisions"
       elsif user.leader? # ...otherwise if they're a leader...
--- spec/models/permission_spec.rb
+++ spec/models/permission_spec.rb
@@ -1,6 +1,5 @@
 require 'rails_helper'
 
-# Allow Checks for authorization of whether user is allowed to do the thing as leader/super/viewer, and for whether they're associated with the circle
 RSpec::Matchers.define :allow do |controller, action|
   match do |permission|
     permission.allow?(controller, action)
@@ -22,30 +21,30 @@ describe Permission do
     it { should allow("sessions", "create") }
     it { should allow("sessions", "destroy") }
 
-    it { should allow("circles", "index") }
-    it { should_not allow("circles", "new") }
-    it { should_not allow("circles", "create") }
-    it { should_not allow("circles", "edit") }
-    it { should_not allow("circles", "update") }
-    it { should_not allow("circles", "destroy") }
-
-    it { should_not allow("users", "new") }
-    it { should_not allow("users", "create") }
-    it { should_not allow("users", "edit") }
-    it { should_not allow("users", "update") }
-    it { should_not allow("users", "destroy") }
+    # it { should allow("circles", "index") }
+    # it { should_not allow("circles", "new") }
+    # it { should_not allow("circles", "create") }
+    # it { should_not allow("circles", "edit") }
+    # it { should_not allow("circles", "update") }
+    # it { should_not allow("circles", "destroy") }
+    #
+    # it { should_not allow("users", "new") }
+    # it { should_not allow("users", "create") }
+    # it { should_not allow("users", "edit") }
+    # it { should_not allow("users", "update") }
+    # it { should_not allow("users", "destroy") }
   end
 
   context 'User as leader' do
     context 'of Circle with Specific Decision' do
 
-      let!(:specific_decision) { create :decision, name: "One" }
-      let!(:circle_with_specific_decision) do
+      let(:specific_decision) { create :decision }
+      let(:circle_with_specific_decision) do
         circle = create(:circle, :with_decisions)
         circle.decisions.push(specific_decision)
         circle
       end
-      let!(:leader_with_specific_circle) do
+      let(:leader_with_specific_circle) do
         leader = create(:leader, :with_many_circles)
         leader.circles.push(circle_with_specific_decision)
         leader
@@ -59,9 +58,57 @@ describe Permission do
       it { should allow("decisions", "edit") }
       it { should allow("decisions", "update") }
       it { should allow("decisions", "destroy") }
+
+      it { should allow("sessions", "new") }
+      it { should allow("sessions", "create") }
+      it { should allow("sessions", "destroy") }
     end
-    # context 'of Circle not having Decision in Question' do
-    #   leaderless_circle = create :circle, name: "Leaderless Circle"
-    # end
+
+    context 'not being involved in Circle of Decision in Question' do
+      let(:specific_decision) { create :decision }
+      let(:circle_with_specific_decision) do
+        circle = create(:circle, :with_decisions)
+        circle.decisions.push(specific_decision)
+        circle
+      end
+      let(:non_leader_of_specific_circle) { create(:leader, :with_many_circles) }
+
+      subject { Permission.new(user: non_leader_of_specific_circle, circle: circle_with_specific_decision, decision: specific_decision) }
+
+      it { should allow("decisions", "index") }
+      it { should allow("decisions", "new") }
+      it { should_not allow("decisions", "create") }
+      it { should_not allow("decisions", "edit") }
+      it { should_not allow("decisions", "update") }
+      it { should_not allow("decisions", "destroy") }
+
+      it { should allow("sessions", "new") }
+      it { should allow("sessions", "create") }
+      it { should allow("sessions", "destroy") }
+    end
+
+    context 'of Circle not having Decision in Question' do
+      let(:specific_decision) { create :decision }
+      let(:circle_with_decisions_but_not_specific_decision) { create :circle, :with_decisions }
+      let(:leader_of_specific_circle) do
+        leader = create(:leader, :with_many_circles)
+        leader.circles.push(circle_with_decisions_but_not_specific_decision)
+        leader
+      end
+
+      subject { Permission.new(user: leader_of_specific_circle, circle: circle_with_decisions_but_not_specific_decision, decision: specific_decision) }
+
+      it { should allow("decisions", "index") }
+      it { should allow("decisions", "new") }
+      it { should_not allow("decisions", "create") }
+      it { should_not allow("decisions", "edit") }
+      it { should_not allow("decisions", "update") }
+      it { should_not allow("decisions", "destroy") }
+
+      it { should allow("sessions", "new") }
+      it { should allow("sessions", "create") }
+      it { should allow("sessions", "destroy") }
+
+    end
   end
 end
