60659182e28e5aed149bc9b18991b631e3ab3a99
ritabc <ritabc@gmail.com>
2020-07-07 00:21:34 -0400

    more changes for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,21 @@ Rails.application.configure do
   # Set host url for mailer
   config.action_mailer.default_url_options = { :host => "http://hmev-decision-log.herokuapp.com" }
 
+  config.action_mailer.perform_deliveries = true
+
+  # Change mail delivery to either :smtp, :sendmail, :file, :test
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "http://hmev-decision-log.herokuapp.com",
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"],
+    authentication: "plain",
+    enable_starttls_auto: true
+    }
+
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
